4a75b0e67664d95a45f889abea33e2a77322bc42
Eman Abdelmohsen <eman.abdelmohsen@roaa.tech>
2021-03-07 02:27:57 +0200

    added service result

---
+++ WebTestingService/Utilities/ServiceResult.cs
@@ -0,0 +1,49 @@
+﻿
+namespace WebTestingService.Utilities
+{
+    public class ServiceResult
+    {
+        public bool Succeeded { get; }
+        public string ErrorMessage { get; }
+        public int ErrorCode { get; }
+
+        public ServiceResult(bool succeeded = true)
+        {
+            Succeeded = succeeded;
+        }
+
+        public ServiceResult(bool succeeded, int errCode, string errMsg)
+        {
+            Succeeded = succeeded;
+            ErrorMessage = errMsg;
+            ErrorCode = errCode;
+        }
+
+        public ServiceResult(int errCode, string errMsg)
+        {
+            Succeeded = false;
+            ErrorMessage = errMsg;
+            ErrorCode = errCode;
+        }
+    }
+
+    public class ServiceResult<T> : ServiceResult
+    {
+        public T Data { get; }
+
+        public ServiceResult(T data) : base()
+        {
+            Data = data;
+        }
+
+        public ServiceResult(bool succeeded, int errCode, string errMsg, T data) : base(succeeded, errCode, errMsg)
+        {
+            Data = data;
+        }
+
+        public ServiceResult(int errCode, string errMsg, T data) : base(errCode, errMsg)
+        {
+            Data = data;
+        }
+    }
+}
