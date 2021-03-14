83dd5a3d52d36894382bcf7ae5dfe49378ded3d4
Eman Abdelmohsen <eman.abdelmohsen@roaa.tech>
2021-03-14 19:48:20 +0200

    added web page link checking service

---
+++ WebTestingService/IWebPageLinkCheckingService.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.ServiceModel;
+using WebTestingService.Utilities;
+
+namespace WebTestingService
+{
+    [ServiceContract]
+    public interface IWebPageLinkCheckingService
+    {
+        [OperationContract]
+        ServiceResult<List<string>> GetAllLinksFromPageUrl(string url);
+
+        [OperationContract]
+        ServiceResult<List<string>> GetAllBrokenLinksFromPageUrl(string url);
+    }
+}
---
+++ WebTestingService/WebPageLinkCheckingService.svc.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using WebTestingService.Utilities;
+
+namespace WebTestingService
+{
+    public class WebPageLinkCheckingService : IWebPageLinkCheckingService
+    {
+        public ServiceResult<List<string>> GetAllLinksFromPageUrl(string url)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public ServiceResult<List<string>> GetAllBrokenLinksFromPageUrl(string url)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
